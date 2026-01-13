b849f778c4cb369ddf4180a74cffee9697de55bf
Gigamoose <boardedmind@gmail.com>
2026-01-13 23:45:37 +0800

    // Refactored everything: Created a class system for ease

--- main.lua
+++ main.lua
@@ -1,22 +1,30 @@
-local Particles = require "source.particles"
-local Player = require "source.player"
-
-local listOfParticles = {}
+local Entities = {} --// table to store all our entities
 
 function love.load()
-    math.randomseed(os.time()) --[for pure randomness since lua uses seed for it]
-    
-    local centerx = love.graphics.getWidth()/2
-    local centery = love.graphics.getHeight()/2
-    Player:setposition(centerx, centery) 
+    --// Requirements
+    local Player = require("source.player")
+
+    --// Screen values
+    local sw = love.graphics.getWidth()
+    local sh = love.graphics.getHeight()
+
+    --// Constructors
+    P1 = Player:new(sw/2, sh/2, 10, 15)
+    Entities[1] = P1
 end
 
 function love.update(dt)
-    Particles.update(listOfParticles, dt)
-    Player:update(dt)
+    
+    --// update all entities in the table 
+    for i, v in ipairs(Entities) do
+        Entities[i]:update(dt)
+    end
 end
 
 function love.draw()
-    Particles.draw(listOfParticles)
-    Player:draw()
+    
+    --// draw all entities in the table 
+    for i, v in ipairs(Entities) do
+        Entities[i]:draw()
+    end
 end
